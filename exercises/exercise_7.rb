a720d44ba3ee8e4168069b8b2be4585a47a8818c
DionKanhai <dionkanhais@gmail.com>
2023-02-02 23:43:18 +0000

    add validation for store and employees

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,8 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+puts "Please enter a store name"
+@store_name = gets.chomp
+
+@store1 = Store.create(name: @store_name)
+puts @store1.errors.full_messages
